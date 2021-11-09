6d56e555054dd651cf8e34e3383a4598eafd81e9
Omar Yousef <71517267+oyousef25@users.noreply.github.com>
2021-11-09 09:54:58 -0500

    Added the required files for assignment 6

---
+++ helper-files/contentDB.ts
@@ -0,0 +1,47 @@
+import {Input} from "@angular/core";
+import {Content} from "./Content";
+
+export const CONTENTARRAY: Content[] = [{
+  id: 1023,
+  author: 'Omar',
+  title: 'Mobile development and the future',
+  body: 'Coding is amazing and stressful',
+  imgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+  type: "creative",
+  tags: ["string"]
+  },
+  {
+    id: 1022,
+    author: 'Tom',
+    title: 'Tom is cool',
+    body: 'He is cool because he is',
+    imgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    type: "horror",
+    tags: ["string"]
+  },
+  {
+    id: 1021,
+    author: 'Dawn',
+    title: 'Dawn is a good person',
+    body: 'She is a good person!',
+    imgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    type: "horror",
+    tags: ["string"]
+  },
+  {
+    id: 1021,
+    author: 'Alex',
+    title: 'Alex is amazing in Js',
+    body: 'We all know that',
+    imgUrl: "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg",
+    type: "comedy",
+    tags: ["string"]
+  },
+  {
+    id: 1021,
+    author: 'Peter',
+    title: 'Peter is smart',
+    body: 'I agree!',
+    type: "comedy",
+    tags: ["hummus", "string", "shawarma"]
+  }];
